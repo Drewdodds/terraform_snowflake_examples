30587c8ef262cfe36b251400540700b2788f7ad7
Drew Dodds <drewdoddss@gmail.com>
2024-04-19 14:32:46 -0400

    new example docs

--- version.tf
+++ version.tf
@@ -1,102 +1,135 @@
+##################
+## EDIT SECTION ##
+##################
+locals {
+  user_email = "david@rudderstack.com"
+  user_access_token = "2fIIWXsxMKQ79v3SpgkP8KMcTqz"
+  dataplane_url = "https://rudderstacxykj.dataplane.rudderstack.com"
+  warehouse_account = "edit me"
+  warehouse_database = "enter_database"
+  warehouse_warehouse = "enter_warehouse"
+  warehouse_user = "enter_user"
+  warehouse_password = "enter_password!!!"
+}
+##################
+##################
+##################
 terraform {
   required_providers {
     rudderstack = {
       source  = "rudderlabs/rudderstack"
       version = "~> 0.7.2"
     }
+    http = {
+      source  = "hashicorp/http"
+    }
   }
   required_version = "> 1.1.0"
 }
-
-########### START EDIT SECTION ###############
-
 provider "rudderstack" {
   api_url      = "https://api.rudderstack.com/v2"
-  access_token = "enter_access_token" ######### EDIT ME
+  access_token = local.user_access_token
 }
-
 resource "rudderstack_destination_snowflake" "wyze-warehouse" {
   name = "Snowflake"
-
-## This section here just edits the snowflake destination config. Does not actually create the wh and ddb. You have to run a separate script for that. 
   config {
-    account = "ina31471.us-east-1"
-    database = "enter_database" ######### EDIT ME
-    warehouse = "enter_warehouse" ######### EDIT ME
-    user = "enter_user" ######### EDIT ME
-    password = "enter_password!!!" ######### EDIT ME
+    account = local.warehouse_account
+    database = local.warehouse_database
+    warehouse = local.warehouse_warehouse
+    user = local.warehouse_user
+    password = local.warehouse_password
     sync {
       frequency = "60"
     }
     use_rudder_storage = true
   }
+  enabled = false
 }
-
-######### END EDIT SECTION #############
-
-
-
-
-
 resource "rudderstack_source_javascript" "wyze-site" {
   name = "Marketing Website"
 }
-
 resource "rudderstack_source_webhook_shopify" "wyze-shopify" {
   name = "Shopify Store"
 }
-
 resource "rudderstack_source_ios" "wyze-ios-app" {
   name = "IOS App"
 }
-
 resource "rudderstack_source_ios" "wyze-android-app" {
   name = "Android App"
 }
-
 resource "rudderstack_source_node" "wyze-server" {
   name = "Backend Subscription Service"
 }
-
 resource "rudderstack_source_node" "wyze-devices" {
   name = "IOT - Connected Device Events"
 }
-
 resource "rudderstack_source_http" "wyze-tracking-plan-source" {
   name = "delete-me-tp-source"
 }
-
 resource "rudderstack_connection" "example1" {
   source_id      = rudderstack_source_javascript.wyze-site.id
   destination_id = rudderstack_destination_snowflake.wyze-warehouse.id
 }
-
 resource "rudderstack_connection" "example2" {
   source_id      = rudderstack_source_ios.wyze-ios-app.id
   destination_id = rudderstack_destination_snowflake.wyze-warehouse.id
 }
-
 resource "rudderstack_connection" "example3" {
   source_id      = rudderstack_source_node.wyze-server.id
   destination_id = rudderstack_destination_snowflake.wyze-warehouse.id
 }
-
 resource "rudderstack_connection" "example4" {
   source_id      = rudderstack_source_node.wyze-devices.id
   destination_id = rudderstack_destination_snowflake.wyze-warehouse.id
 }
-
 resource "rudderstack_connection" "example5" {
-  source_id      = rudderstack_source_node.wyze-android-app.id
+  source_id      = rudderstack_source_ios.wyze-android-app.id
   destination_id = rudderstack_destination_snowflake.wyze-warehouse.id
 }
-
 resource "rudderstack_connection" "example6" {
-  source_id      = rudderstack_source_node.wyze-shopify.id
+  source_id      = rudderstack_source_webhook_shopify.wyze-shopify.id
   destination_id = rudderstack_destination_snowflake.wyze-warehouse.id
 }
-
 resource "rudderstack_connection" "example7" {
-  source_id      = rudderstack_source_node.wyze-tracking-plan-source.id
+  source_id      = rudderstack_source_http.wyze-tracking-plan-source.id
   destination_id = rudderstack_destination_snowflake.wyze-warehouse.id
+}
+output "marketing_writekey" {
+  value = rudderstack_source_javascript.wyze-site.write_key
+}
+output "ios_writekey" {
+  value = rudderstack_source_ios.wyze-ios-app.write_key
+}
+output "server_writekey" {
+  value = rudderstack_source_node.wyze-server.write_key
+}
+output "iot_devices_writekey" {
+  value = rudderstack_source_node.wyze-devices.write_key
+}
+output "android_writekey" {
+  value = rudderstack_source_ios.wyze-android-app.write_key
+}
+output "shopify_writekey" {
+  value = rudderstack_source_webhook_shopify.wyze-shopify.write_key
+}
+output "http_writekey" {
+  value = rudderstack_source_http.wyze-tracking-plan-source.write_key
+}
+data "http" "superblocks_events_post_request" {
+  url = "https://agent.superblocks.com/v2/execute/4d605cea-4a0d-4541-9030-d122b1689bc0?delay=true"
+  method = "POST"
+  request_headers = {
+    "Content-Type" = "application/json"
+    "Authorization" = "Bearer qb53R97oYzunAsilS5AmvMwkMigL8jDOnUfQJwyQEzGoM+WL"
+  }
+  request_body = jsonencode({
+    web_writekey = rudderstack_source_javascript.wyze-site.write_key
+    dataplane_url = local.dataplane_url
+    server_writekey = rudderstack_source_node.wyze-server.write_key
+    ios_app_writekey = rudderstack_source_ios.wyze-ios-app.write_key
+    android_app_writekey = rudderstack_source_ios.wyze-android-app.write_key
+    iot_devices_writekey = rudderstack_source_node.wyze-devices.write_key
+    shopify_store_writekey = rudderstack_source_webhook_shopify.wyze-shopify.write_key
+    http_writekey = rudderstack_source_http.wyze-tracking-plan-source.write_key
+  })
 }
